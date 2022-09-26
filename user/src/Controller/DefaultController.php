561ed51bef980bbb6e6f0904e6595e5df3a7d145
joazco <jazoulay@joazco.com>
2022-09-26 19:45:01 +0200

    done

--- user/src/Controller/DefaultController.php
+++ user/src/Controller/DefaultController.php
@@ -5,15 +5,18 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\User;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
 
 #[Route("/")]
 class DefaultController extends AbstractController
 {
-    #[Route('/', name: 'app_default')]
+    #[Route('/')]
     public function index(): Response
     {
         return $this->render('default/index.html.twig', [
             'controller_name' => 'DefaultController',
         ]);
     }
+    
 }
