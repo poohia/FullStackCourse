5fe2b242868f0aaf8a3318dc44bb36ec6e790101
joazco <jazoulay@joazco.com>
2022-09-27 15:50:34 +0200

    append body parser

--- proxy/src/app.ts
+++ proxy/src/app.ts
@@ -1,9 +1,16 @@
 import express from "express";
 import axios from "axios";
+import bodyParser from "body-parser";
 import { endPointServiceUserHello, urlServiceUser } from "./types";
 
 const app = express();
 const port = 8000;
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
 
 app.get("/api", (_, res) => {
   res.send("Hello API");
