4a1e2b88713924f7b0974230f7c20d6bc0c986f7
Jordan <jazoulay@joazco.com>
2023-02-12 14:14:33 +0100

    solution exercice n°2

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 // https://www.electronjs.org/docs/latest/
-const { app, BrowserWindow, ipcMain, dialog, shell } = require("electron");
+const {
+  app,
+  BrowserWindow,
+  ipcMain,
+  dialog,
+  shell,
+  nativeTheme,
+  Notification,
+} = require("electron");
 const path = require("path");
 const fs = require("fs");
 const { execFile } = require("node:child_process");
@@ -13,7 +21,9 @@ function accessOrCreateFolder(folderPath, mode = fs.constants.W_OK) {
   return new Promise((resolve, reject) => {
     const normalizePath = path.normalize(folderPath);
     fs.access(normalizePath, mode, (err) => {
+      // err is null if normalizePath dosn't exist
       if (err) {
+        // then create it
         fs.mkdir(normalizePath, (err) => {
           if (err) {
             reject();
@@ -37,11 +47,13 @@ function createWindow() {
 
   // listen event ping from front and reply pong
   ipcMain.on("ping", (event) => {
-    console.log("ping\n\n\n");
+    console.log("ping\n\n");
     event.reply("pong");
   });
 
   ipcMain.on("openFile", () => {
+    let progress = 0;
+    mainWindow.setProgressBar(progress);
     // dialog.showOpenDialog Open a dialog to select a file
     // In this example only files with extension ".png"
     dialog
@@ -51,28 +63,61 @@ function createWindow() {
       })
       .then((result) => {
         accessOrCreateFolder(targetFolder).then(() => {
-          result.filePaths.forEach((filePath) => {
-            const fileName = path.basename(filePath);
-            // compress image
-            execFile(
-              // pngquant is software to compress image
-              pngquant,
-              [
-                "--quality=90-100",
-                "-o",
-                `${targetFolder}/${fileName}`,
-                filePath,
-              ],
-              () => {}
-            );
+          // Transform all file path to Promise and then is all promise are resolved continue script
+          // https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Global_Objects/Promise/all
+          Promise.all(
+            // Map transform array to other one
+            // https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Global_Objects/Array/map
+            result.filePaths.map(
+              (filePath, i) =>
+                new Promise((resolve) => {
+                  const fileName = path.basename(filePath);
+                  // compress image
+                  execFile(
+                    // pngquant is software to compress image
+                    pngquant,
+                    [
+                      "--quality=90-100",
+                      "-o",
+                      `${targetFolder}/${fileName}`,
+                      filePath,
+                    ],
+                    () => {
+                      // add progress and resolve promess i * 1000 timeout
+                      setTimeout(() => {
+                        progress += 100 / result.filePaths.length / 100;
+                        mainWindow.setProgressBar(progress);
+                        resolve();
+                      }, i * 1000);
+                    },
+                    () => {
+                      // in error case just resolve promess
+                      resolve();
+                    }
+                  );
+                })
+            )
+            // Then all promesse are resolved
+          ).then(() => {
+            // Show notification
+            new Notification({
+              title: "Compresseur PNG",
+              body: "Compression finit",
+            }).show();
+            // Open temp folder
+            shell.openPath(targetFolder);
+            // Reset progress bar
+            mainWindow.setProgressBar(-1);
           });
-          // open folder from path
-          shell.openPath(targetFolder);
         });
       });
   });
 
   mainWindow.loadFile("index.html");
+  // active darkmode
+  nativeTheme.themeSource = "dark";
+  // active maximize size window
+  mainWindow.maximize();
 }
 
 app.whenReady().then(() => {
